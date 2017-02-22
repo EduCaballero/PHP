19ae40bd3fde0bf958a8deba1c22c625741094d9
EduStrife <47883089x@NOVAULAS.STUCOM.COM>
2017-02-22 20:10:07 +0100

    Stukemon Go

---
+++ StukemonGo/index.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <header><h1>Bienvenido a Stukemon Go!</h1></header>
+        <div>
+            <div><a href="nuevoEntrenador.php">Dar de alta entrenador</a></div>
+            <div><a href="nuevoPokemon.php">Dar de alta un Pokémon</div>
+            <div><a href="batalla.php">Batalla</a></div>
+            <div><a href="mejorarVida.php">Mejorar vida</a></div>
+            <div><a href="pociones.php">Conseguir pociones de salud</a></div>
+            <div><a href="listaPokemon.php">Listado de Pokémon</a></div>
+            <div><a href="rankingEntrenadores.php">Ranking entrenadores</a></div>
+            <div><a href="RankingBatallas.php">Ranking de batallas</a></div>
+        </div>
+        <?php        
+        ?>
+    </body>
+</html>
